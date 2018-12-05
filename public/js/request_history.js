d4ca24a947b33c02257050ed4289533f22676ae6
radulko86 <radulko86@gmail.com>
2018-12-05 16:42:12 +0100

    off key from history

--- public/js/request_history.js
+++ public/js/request_history.js
@@ -1,30 +1,57 @@
 ;(function(global, $){
-    function Credentials(cpe,wc,mip){
+    function Credentials(cpe,wc,mip, type){
         this.cpe = cpe;
         this.wc = wc;
         this.mip = mip;
+        this.type = type;
     }
     Credentials.prototype.sendRequest = function(){
-        $.ajax({
-            url: '/copyoff',
-            method: 'POST',
-            data: {
-                cpe: this.cpe,
-                wc: this.wc,
-                mip: this.mip,
-                requestType: 'aquilaOffKey'
-            }
-        })
+       
+            $.ajax({
+                url: "/newrequest",
+                method: "POST",
+                data: {
+                    modemIP     :   this.mip,
+                    wc          :   this.wc,
+                    cpeIP       :   this.cpe,
+                    modemType   :   this.type,
+                    requestType :   'aquilaOffKey'
+                },
+                dataContent: "application/json",
+                success: function(data, status, jqXHR){
+                    //do something with returned key
+                    // console.log(data);
+                    //insert returned string into element
+                    //need to be textarea element in order for the copy to work
+                    $('#request_result_off').text(data);
+                    $('.off_container').html('<div class="ui red tiny button" id="off_key">Off key</div>'); 
+                }
+            })
+      
+        
     }
-    $('.still_on').on('click', function(){
-        var cpe = $(this).find('td.cpeip').html(),
-            wc  = $(this).find('td.wc').html()
-            mip = $(this).find('td.modemip').html();
 
-        var newRequest = new Credentials(cpe, wc, mip);
+    //CONTINUE HERE!!!!!
+    $('.still_on').on('click', function(){
+        if ($(this).find('#off_key')) {
+            return
+        }
+        var cpe = $(this).find('td.cpeip').html(),
+            wc  = $(this).find('td.wc').html(),
+            mip = $(this).find('td.modemip').html(),
+            type = $(this).find('td.type').html();
+
+        var newRequest = new Credentials(cpe, wc, mip, type);
         
         newRequest.sendRequest();
     })
+  
+    $('.off_container').on('click', '#off_key', function(e){
+        new Clipboard('.copy');
+        $('#off_key').html('Closed'); 
+
+    })    
 })(window, jQuery)
     
    
+
